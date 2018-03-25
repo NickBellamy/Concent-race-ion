e97da03881fff2c722f6017e6d6ea77dc960ddfd
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-03-25 13:38:12 +0100

    feat: Add function to deal cards
    The first 8 cards from the shuffled array of cards will be added to two
    randomly shuffled locations.

--- js/app.js
+++ js/app.js
@@ -5,11 +5,14 @@ document.querySelector('#deck').addEventListener('click', function(e){
     }
 });
 
-let cards = [0,1,2,3,4,5,6,7]
-let locations = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+// Can be any length - only 8 will be chosen
+let cards = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+// Must be 16 long as each number referes to a location in the grid
+let locations = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 
 cards = shuffle(cards);
 locations = shuffle(locations);
+deal(cards, locations);
 
 // Randomises the order of items in an array using the Fisher-Yates shuffle
 // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
@@ -24,6 +27,17 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-    
+
     return array;
 } 
+
+// Deal 2 of each of the first 8 cards in cards, to each location in locations
+function deal(cards, locations) {
+    for(let i=0; i < 8; i++) {
+        const currentCard = cards[i];
+        const locationOne = document.querySelector('.card_container:nth-child('+locations[i] + ')');
+        const locationTwo = document.querySelector('.card_container:nth-child('+locations[i + 8] + ')');
+        locationOne.querySelector('.back').textContent = currentCard;
+        locationTwo.querySelector('.back').textContent = currentCard;
+    }
+}
