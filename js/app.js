a7cd087f1015441e626305b57c97230e2d5de9f7
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-03-25 15:42:35 +0100

    feat: Prevent cards flipping face down on click

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Flip card over on click
 document.querySelector('#deck').addEventListener('click', function(e) {
     if (e.target.classList.contains("front")) {
-        e.target.parentNode.classList.toggle("flipped");
+        e.target.parentNode.classList.add("flipped");
     }
 });
 
